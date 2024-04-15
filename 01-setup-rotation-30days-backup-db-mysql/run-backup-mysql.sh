16bcf34fb55dab278b7c5d546ce7bbcc4671867e
Benjaman Tran <t16021999@gmail.com>
2024-04-15 10:58:58 +0700

    done issue

---
+++ 01-setup-rotation-30days-backup-db-mysql/run-backup-mysql.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+### System Setup ###
+NOW=$(date +%Y-%m-%d)
+KEEPDAYS=30
+MUSER="username" # XXXX is mt gs number
+MPASS="password" # set to MySQL password
+MHOST="host"     # XXXX is mt gs number
+DBS="db_name"    # space separated list of databases to backup
+MDIR="/home/usr/backup"
+EMAILID="youremail@gmail.vn" # the email you want notification sent to
+attempts=0
+for db in $DBS; do                     # for each listed database
+	attempts=$(expr $attempts + 1)        # count the backup attempts
+	FILE=$MDIR/db-redmine-$db.$NOW.sql.gz # Set the backup filename
+	mysqldump -q -u $MUSER -h $MHOST -p$MPASS $db | gzip -9 >$FILE
+done
+find $MDIR/db-redmine*.sql.gz -type f -daystart -mtime +$KEEPDAYS -exec rm {} \;
+
+localfiles=$(ls $MDIR/*.$NOW.sql.gz)
+count=0 # count local files from today
+for file in $localfiles; do count=$(expr $count + 1); done
+mail -s "Redmine Backups Report $NOW" $EMAILID <<END
+Success with $count of $attempts
+The following databases were attempted backed up
+$DBS
+Files stored:
+$localfiles
+END
+
+# set up crontab -e * 8 1,2,3,4,5 * * /bin/bash mysql_backup.sh
